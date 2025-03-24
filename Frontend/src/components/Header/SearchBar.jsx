2342070dba37593a41587903c0f01ce44f3e1335
jaimin0609 <jaimin0609@gmail.com>
2025-03-24 12:27:07 +0800

    fix: change productService import to use default import

--- Frontend/src/components/Header/SearchBar.jsx
+++ Frontend/src/components/Header/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { productService } from '../../services/productService';
+import productService from '../../services/productService';
 import {
     MagnifyingGlassIcon,
     XMarkIcon
